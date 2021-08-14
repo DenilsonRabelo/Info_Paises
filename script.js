7bc183f16167c79ff9e354fedc726e4062eca86d
Denilson Rabelo <80592413+DenilsonRabelo@users.noreply.github.com>
2021-08-14 02:04:02 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -49,17 +49,19 @@ window.onload = function () {
 
         function addData(data, indica) {
             for (const i in data) {
-                const add = document.createElement('option')
-                add.textContent = Object.keys(data[i])
-                document.getElementById('77818').appendChild(add)
-                add.setAttribute('value', Object.keys(indica.series['0']['serie'][i]))
+                if (!(Object.values(data[i]) === null || Object.values(data[i]) == '')) {
+                    const add = document.createElement('option')
+                    add.textContent = Object.keys(data[i])
+                    document.getElementById('77818').appendChild(add)
+                    add.setAttribute('value', Object.keys(indica.series['0']['serie'][i]))
+                } 
             }
         }
 
         function addValores(indica) {
             for (const i in indica) {
                 if (document.getElementById('77818').value == Object.keys(indica[i])) {
-                    if (Object.keys(indica[i]) != '-' && Object.values(indica[i]) == ''|| Object.values(indica[i]) === null) {
+                    if (Object.keys(indica[i]) == '-' && Object.values(indica[i]) == '' || Object.values(indica[i]) === null) {
                         document.getElementById('economia1').value = 'Sem dados para esse ano'
                     } else {
                         document.getElementById('economia1').value = Object.values(indica[i])
