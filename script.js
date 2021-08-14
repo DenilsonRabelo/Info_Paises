520a81e6745baf603d7843a2f2600d0a90d117e0
Denilson Rabelo <80592413+DenilsonRabelo@users.noreply.github.com>
2021-08-14 23:48:55 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -47,40 +47,38 @@ window.onload = function () {
             })
 
 
-        function addData(data, indica) {
-            for (const i in data) {
-                if (!(Object.values(data[i]) === null || Object.values(data[i]) == '')) {
-                    const add = document.createElement('option')
-                    add.textContent = Object.keys(data[i])
-                    document.getElementById('77818').appendChild(add)
-                    add.setAttribute('value', Object.keys(indica.series['0']['serie'][i]))
-                } 
-            }
-        }
 
-        function addValores(indica) {
-            for (const i in indica) {
-                if (document.getElementById('77818').value == Object.keys(indica[i])) {
-                    if (Object.keys(indica[i]) == '-' && Object.values(indica[i]) == '' || Object.values(indica[i]) === null) {
-                        document.getElementById('economia1').value = 'Sem dados para esse ano'
-                    } else {
-                        document.getElementById('economia1').value = Object.values(indica[i])
-                    }
-                }
-            }
-        }
 
 
         //criando datas do select como opções
-        fetch(`https://servicodados.ibge.gov.br/api/v1/paises/${select.options[select.selectedIndex].value}/indicadores/${document.getElementById('77818').id}`)
+        fetch(`https://servicodados.ibge.gov.br/api/v1/paises/${select.options[select.selectedIndex].value}/indicadores/`)
             .then(respota => respota.json())
             .then(data => {
                 data.map(indicadores => {
-                    let datas = { ...Object.values(indicadores.series['0']['serie']) }
-                    addData(datas, indicadores)
-                    document.getElementById('77818').addEventListener('click', () => {
-                        addValores(datas)
-                    })
+                    const lf = document.getElementById('ac')
+                    let add = document.createElement('select')
+                    add.setAttribute('id', indicadores.id)
+                    lf.appendChild(add)
+
+
+
+                    const lp = document.getElementById(`${indicadores.id}`)
+                    for (const i in Object.values(indicadores.series['0'].serie)) {
+                        const option = document.createElement('option')
+                        option.textContent = Object.keys(indicadores.series['0'].serie[i])
+                        lp.appendChild(option)
+                        const label = document.createElement('label')
+                        label.setAttribute('for', indicadores.id)
+                        lp.appendChild(label)
+                        const input = document.createElement('input')
+                        input.setAttribute('type', 'text')
+                        lp.appendChild(input)
+                        input.setAttribute('id', indicadores.id)
+                        lp.appendChild(input)
+                    }
+
+
+
                 })
             })
     })
