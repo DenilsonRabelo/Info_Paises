5b84a2cede1f6aa0e0f32bdd064dbe70dcfcd329
Denilson Rabelo <80592413+DenilsonRabelo@users.noreply.github.com>
2021-08-14 01:04:26 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -35,7 +35,6 @@ window.onload = function () {
             .then(res => res.json())
             .then(data => {
                 data.map(p => {
-
                     let subloc = refatoraJson(p)
                     document.getElementById('area').value = p['area']['total'] + " km²"
                     document.getElementById('localizacao').value = p['localizacao']['regiao']['nome']
@@ -44,6 +43,42 @@ window.onload = function () {
                     document.getElementById('sub-regiao').value = subloc.localizacao['subregiao']['nome']
                     document.getElementById('historico').value = p['historico']
                     document.getElementById('unidades-monetarias').value = subloc.unidadesmonetarias['0']['id']['ISO4217ALPHA'] + ' - ' + subloc.unidadesmonetarias['0']['nome']
+                })
+            })
+
+
+        function addData(data, indica) {
+            for (const i in data) {
+                const add = document.createElement('option')
+                add.textContent = Object.keys(data[i])
+                document.getElementById('77818').appendChild(add)
+                add.setAttribute('value', Object.keys(indica.series['0']['serie'][i]))
+            }
+        }
+
+        function addValores(indica) {
+            for (const i in indica) {
+                if (document.getElementById('77818').value == Object.keys(indica[i])) {
+                    if (Object.keys(indica[i]) != '-' && Object.values(indica[i]) == ''|| Object.values(indica[i]) === null) {
+                        document.getElementById('economia1').value = 'Sem dados para esse ano'
+                    } else {
+                        document.getElementById('economia1').value = Object.values(indica[i])
+                    }
+                }
+            }
+        }
+
+
+        //criando datas do select como opções
+        fetch(`https://servicodados.ibge.gov.br/api/v1/paises/${select.options[select.selectedIndex].value}/indicadores/${document.getElementById('77818').id}`)
+            .then(respota => respota.json())
+            .then(data => {
+                data.map(indicadores => {
+                    let datas = { ...Object.values(indicadores.series['0']['serie']) }
+                    addData(datas, indicadores)
+                    document.getElementById('77818').addEventListener('click', () => {
+                        addValores(datas)
+                    })
                 })
             })
     })
